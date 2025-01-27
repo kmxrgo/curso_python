0f0694723d4500daf5891c5c445ab4a5f5a466bd
miguelandloaiza30@gmail.com <miguelandloaiza30@gmail.com>
2025-01-27 14:29:38 -0700

    Cree el archivo de funciones auxiliares para "Linea"

---
+++ Linea/funciones.py
@@ -0,0 +1,21 @@
+'''
+Funciones auxiliares para el programa "Linea"
+'''
+
+def calcular_y(x,m,b):
+    '''
+      Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b"
+      Retorna el valor de "y"
+    '''
+
+    return m*x+b
+
+if __name__ == "_main_":
+    x = 0
+    m = 3
+    b = 2
+    y = calcular_y(x,m,b)
+    if y== 2:
+        print("Prueba Exitosa")
+    else:
+        print("Prueba Fallida")
