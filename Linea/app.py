e1b052d09fe6615ce3eb5910c239350c714c553e
miguelandloaiza30@gmail.com <miguelandloaiza30@gmail.com>
2025-01-28 14:54:51 -0700

    Agregue la funcion graficar linea

--- Linea/app.py
+++ Linea/app.py
@@ -1,16 +1,23 @@
-'''
-    Archivo principal de Linea
-'''
+'''Calculo de coordenadas de Lineas'''
+import argparse
 import funciones
 
-def main(m, b):
-    X = [x/10.5 for x in range (1,101,10)]
-    Y = [funciones.calcular_y(x,m,b) for x in X]
-    print(X)
-    print(Y)
+def main(m:float, b:float):
+    '''
+    Función principal que calcula las coordenadas de una línea recta
+    Recibimos m y b
+    Regresa: nada
+    '''
+    X = [x/10.5 for x in range (1,110,5)]
+    Y = [funciones.calcular_y(x, m, b) for x in X]
     coordenadas = list(zip(X,Y))
     print(coordenadas)
+    funciones.grafica_linea(X,Y,m,b)
 
-if __name__ == "_main_":
-    main(2,3)
-    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calcula las coordenadas de una línea recta')
+    parser.add_argument('-m', type=float, help='Pendiente de la línea', default=2.0)
+    parser.add_argument('-b', type=float, help='Intersección en y', default=3.0)
+    args = parser.parse_args()
+    main(args.m, args.b)
+    #main(m=2.0, b=3.0)
--- Linea/funciones.py
+++ Linea/funciones.py
@@ -1,21 +1,40 @@
-'''
-    Funciones auxiliares para el programa "Linea"
-'''
+''' archivo con todas las funciones necesarias para la aplicación "linea" '''
+import matplotlib.pyplot as plt
 
-def calcular_y(x,m,b):
+def calcular_y(x:float, m:float ,b:float)->float:
     '''
-        Calcula "y" de acuerdo a la pendiente "m" y el punto de intersección en y "b"
-        Retorna el valor de "y"
+        Calcula el valor de y en una línea recta
+        x: valor de x
+        m: pendiente
+        b: intersección en y regresa el valor de y
     '''
 
-    return m*x+b
+    return (m*x)+b
+
+def grafica_linea(X: list, Y: list, m:float, b:float):
+    '''
+    Grafica una linea recta
+    X: lista de valores de x
+    Y: lista de valores de y
+    m: pendiente
+    b: intersección en y
+    '''
+    plt.plot(X,Y)
+    plt.title(f'Linea recta con pendiente={m} e intersección en y={b}')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.show()
+
+def test_linea():
+    '''
+    Comprobamos calcular_y()
+    '''
+    y = calcular_y(0.0, 2.0, 3.0)
+    return y
 
 if __name__ == "_main_":
-    x = 0
-    m = 3
-    b = 2
-    y = calcular_y(x,m,b)
-    if y== 2:
-        print("Prueba Exitosa")
+    if test_linea() == 3.0:
+        print("Test Exitoso")
     else:
-        print("Prueba Fallida")
+        print("Test Fallido")
