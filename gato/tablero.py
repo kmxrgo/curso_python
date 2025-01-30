c9f896a22ea73d6bd1dbe9a50073cdfa95b93947
miguelandloaiza30@gmail.com <miguelandloaiza30@gmail.com>
2025-01-30 14:48:48 -0700

    Agregamos la lista de combinaciones

--- gato/tablero.py
+++ gato/tablero.py
@@ -22,11 +22,48 @@ def ia(simbolos:dict):
             simbolos[x] = 'O'
             ocupado = False
 
+def usuario(simbolos:dict):
+    ''' Estrategia del usuario '''
+    ocupado = True
+    lista_numeros = [str(i) for i in range(1,10)] #del 1 al 9
+    while ocupado is True:
+        x = input('Eliga un número del 1 al 9: ')
+        if x in lista_numeros:
+            if simbolos[x] not in ['X', 'O']:
+                simbolos[x] = 'X'
+                ocupado = False
+            else:
+                print('Esa casilla ya está ocupada')
+
+def juego(simbolos:dict):
+    ''' Juego del gato '''
+    lista_combinaciones = [
+      ['1', '2', '3'],
+      ['4', '5', '6'],
+      ['7', '8', '9'],
+
+      ['1', '4', '7'],
+      ['2', '5', '8'],
+      ['3', '6', '9'],
+
+      ['1', '5', '9'],
+      ['3', '5', '7'],
+    ]
+
+def checa_winner(simbolos:dict, combinaciones:list):
+    ''' Checa si hay un ganador '''
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos[c[2]]:
+            return simbolos[c[0]]
+    return None
+
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
     dibuja_tablero(dsimbolos)
     ia(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    usuario(dsimbolos)
     dibuja_tablero(dsimbolos)
     '''
     x = random.choice(numeros)
@@ -39,4 +76,3 @@ if __name__ == '__main__':
     dibuja_tablero(dsimbolos)
     print(numeros)
     '''
-
