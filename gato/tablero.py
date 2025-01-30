5fc9fd33f61d3560d9b935f9bfdf7a6418cc75cc
miguelandloaiza30@gmail.com <miguelandloaiza30@gmail.com>
2025-01-30 14:22:28 -0700

    Agregué la estrategia de la computadora ia()

--- gato/tablero.py
+++ gato/tablero.py
@@ -13,10 +13,22 @@ def dibuja_tablero(simbolos:dict):
     {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
     ''')
 
+def ia(simbolos:dict):
+    ''' Estrategia de la computadora '''
+    ocupado = True
+    while ocupado is True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ['x', 'O']:
+            simbolos[x] = 'O'
+            ocupado = False
+
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
     dibuja_tablero(dsimbolos)
+    ia(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    '''
     x = random.choice(numeros)
     numeros.remove(x)
     dsimbolos[x] = 'X'
@@ -26,3 +38,5 @@ if __name__ == '__main__':
     dsimbolos[o] = 'O'
     dibuja_tablero(dsimbolos)
     print(numeros)
+    '''
+
