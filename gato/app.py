5762c2085f4a7c68df97b50f4eb1be28887f33c6
miguelandloaiza30@gmail.com <miguelandloaiza30@gmail.com>
2025-02-06 14:30:03 -0700

    Refactor game loop and add main application entry point

---
+++ gato/app.py
@@ -0,0 +1,28 @@
+'''Este archivo es el punto de entrada a la aplicacion. Aqui se importan las funciones de tablero.py y se ejecutan en un ciclo while.'''
+ 
+import tablero
+ 
+def main():
+    ''' Punto de entrada a la aplicacion '''
+    X = {"G":0,"P":0,"E":0}
+    O = {"G":0,"P":0,"E":0}
+    score = {"X":X,"O":O}
+    numeros = {str(i):str(i) for i in range(1,10)}
+    corriendo = True
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = tablero.juego(dsimbolos)
+        if g is not None:
+            print(f'El ganador es {g}')
+            if g == 'X':
+                X["G"] += 1
+            elif g == 'O':
+                O["G"] += 1
+        else:
+            print('Empate')
+            X["E"] += 1
+            O["E"] += 1
+       
+ 
+if __name__ == '__main__':
+    main()
--- gato/tablero.py
+++ gato/tablero.py
@@ -49,32 +49,34 @@ def juego(simbolos:dict):
       ['1', '5', '9'],
       ['3', '5', '7'],
     ]
-
+    
     en_juego = True
     dibuja_tablero(simbolos)
+    movimientos = 0
+    gana = None
     while en_juego:
         usuario(simbolos)
         dibuja_tablero(simbolos)
         movimientos += 1
-        gana = checa_winner(simbolos, lista_combinaciones)
+        gana = checa_winner(simbolos,lista_combinaciones)
         if gana is not None:
             en_juego = False
+            continue
         if movimientos >= 9:
             en_juego = False
             continue
         ia(simbolos)
         dibuja_tablero(simbolos)
         movimientos += 1
-        if movimientos >= 9:
-            en_juego = False
-        gana = checa_winner(simbolos, lista_combinaciones)
+        gana = checa_winner(simbolos,lista_combinaciones)
         if gana is not None:
             en_juego = False
+            continue
         if movimientos >= 9:
             en_juego = False
             continue
+    return gana
         
-
 def checa_winner(simbolos:dict, combinaciones:list):
     ''' Checa si hay un ganador '''
     for c in combinaciones:
@@ -85,7 +87,11 @@ def checa_winner(simbolos:dict, combinaciones:list):
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
-    juego(dsimbolos)
+    g = juego(dsimbolos)
+    if g is not None:
+        print(f'El ganador es {g}')
+    else:
+        print('Empate')
     '''
     dibuja_tablero(dsimbolos)
     ia(dsimbolos)
