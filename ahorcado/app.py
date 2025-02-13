0cf5c667b044d283c418288995fe8e5ee49305a0
miguelandloaiza30@gmail.com <miguelandloaiza30@gmail.com>
2025-02-13 14:50:30 -0700

    función main() de app, con error al restar oportunidades

---
+++ ahorcado/app.py
@@ -0,0 +1,32 @@
+'''Programa principal del juego del ahorcado'''
+
+import string
+import unicodedata
+from random import choice
+import funciones as fn
+
+def main(archivo_texto:str, nombre_plantilla='plantilla'):
+
+    '''Programa principa del juego del ahorcado'''
+
+    # cargamos las plantillas
+    plantillas = fn.carga_plantillas(nombre_plantilla)
+    # cargamos las oraciones
+    oraciones = fn.carga_archivo_texto(archivo_texto)
+    # obtenemos las palabras
+    palabras = fn.obten_palabras(oraciones)
+    o = 5 # oportunidades de fallar
+    p = choice(palabras) # elegimos una palabra al azar
+    abdcario = {letra:letra for letra in string.ascii_lowercase}
+    adivinadas = set()
+    while o > 0:
+        fn.despliega_plantilla(plantillas, o)
+        fn.adivina_letra(abdcario, p, adivinadas, o)
+        if p == ''.join([letra if letra in adivinadas else '_' for letra in p]):
+            print('Felicidades, adivinaste la palabra')
+            break
+    print(f'La palabra era: {p}')
+
+if __name__ == '__main__':
+    archivo = './datos/pg15532.txt'
+    main(archivo)
--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -1,66 +1,68 @@
 '''
-funciones auxiliares del juego Ahorcado
+funciones auxilaries para el juego del ahorcado
 '''
 import string
 import unicodedata
 from random import choice
 
 def carga_archivo_texto(archivo:str)->list:
-    '''
-    Carga un archivo de texto y devuelve una lista con las oraciones del archivo
-    '''
-    with open(archivo, 'r',encoding='utf-8') as file:
-        oraciones = file.readlines()
+    '''Carga un arcihivo de texto y devuelve una lista con las oraciones del archivo'''
+
+    with open(archivo, 'r', encoding='utf-8') as f:
+        oraciones = f.readlines()
     return oraciones
 
-def carga_plantillas(nombre_plantilla:str)->dict:
+
+def carga_plantillas(nombre_plantilla:str) -> dict:
     '''
-    Carga las plantillas del juego apartir de un archivo de texto
+    Carga las plantillas del juego a partir de un archivode texto
     '''
     plantillas = {}
     for i in range(6):
-        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+        plantillas[i] = carga_archivo_texto(f'./ahorcado/plantillas/{nombre_plantilla}-{i}.txt')
     return plantillas
 
 def despliega_plantilla(diccionario:dict, nivel:int):
     '''
     Despliega una plantilla del juego
     '''
+
     if nivel in diccionario:
         template = diccionario[nivel]
         for renglon in template:
             print(renglon)
 
-def obten_palabras(lista:list)->list:
+def obten_palabras(lista:str)->list:
     '''
     Obtiene las palabras de un texto
     '''
     texto = ' '.join(lista[120:])
     palabras = texto.split()
-    # convertimos a minusculas
     minusculas = [palabra.lower() for palabra in palabras]
     set_palabras = set(minusculas)
-    # removemos signos de puntuación y caracteres especiales
+    #removemos signos de puntuacion y caracteres especiales
     set_palabras = {palabra.strip(string.punctuation) for palabra in set_palabras}
-    # removemos números, paréntesis, corchetes y otros caracteres
+    #removemos números, paréntesis, corchetes y otros caracteres
     set_palabras = {palabra for palabra in set_palabras if palabra.isalpha()}
-    # removemos acentos
+    #removemos acentos
     set_palabras = {unicodedata.normalize('NFKD', palabra).encode('ascii', 'ignore').decode('ascii') for palabra in set_palabras}
     return list(set_palabras)
 
-def adivina_letra(abc:dict, palabra:str,letras_adivinadas:set,turnos:int):
+def adivina_letra(abc:dict, palabra:str, letras_adivinadas:set, turnos:int):
     '''
     Adivina una letra de una palabra
     '''
-    palabra_oculta = ""
+
+    palabra_oculta = ''
     for letra in palabra:
         if letra in letras_adivinadas:
             palabra_oculta += letra
         else:
             palabra_oculta += "_"
-    print(f'Tienes {turnos} turnos')
-    print(f'Palabra oculta: {palabra_oculta}')
-    print(f'El abecedario es: {abc}')
+    print(f'Tienes {turnos} oportunidades de fallar')
+    abcd = ' '.join(abc.values())
+    print(f'El abecedario es: {abcd}')
+    print(f'La palabra es: {palabra_oculta}')
     letra = input('Ingresa una letra: ')
     letra = letra.lower()
     if letra in abc:
@@ -71,20 +73,20 @@ def adivina_letra(abc:dict, palabra:str,letras_adivinadas:set,turnos:int):
             if letra in palabra:
                 letras_adivinadas.add(letra)
             else:
-                turnos -= 1
-    
-    
+                turnos -= 1       
+
 
 if __name__ == '__main__':
     plantillas = carga_plantillas('plantilla')
     despliega_plantilla(plantillas, 5)
-    lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+    lista_oraciones = carga_archivo_texto('./ahorcado/datos/pg15532.txt')
     lista_palabras = obten_palabras(lista_oraciones)
     print(len(lista_palabras))
     p = choice(lista_palabras)
     print(p)
     abcdario = {letra:letra for letra in string.ascii_lowercase}
-    letras_adivinadas = set()
-    t = 5 # oportunidades
-    adivina_letra(abcdario, p, letras_adivinadas, t)
-    adivina_letra(abcdario, p, letras_adivinadas, t)
+    adivinadas = set()
+    t = 5 #oportunidades
+    adivina_letra(abcdario, p, adivinadas, t)
+    adivina_letra(abcdario, p, adivinadas, t)
+
